f7afe1dd7432c4869afea64be7036adc6c280ce3
Igordcs <igorcostamdt@gmail.com>
2024-09-23 15:38:05 -0300

    fix

--- src/components/Reminder/ReminderForm.jsx
+++ src/components/Reminder/ReminderForm.jsx
@@ -1,11 +1,15 @@
 const ReminderForm = ({ formData, setFormData, reminderType }) => {
   formData.type = formData.type || reminderType;
-  formData.date = formData.date ? new Date(formData.date).toISOString().split("T")[0] : "";
+  formData.date = formData.date
+    ? new Date(formData.date).toISOString().split("T")[0]
+    : "";
 
   return (
     <div className="reminder-form">
       <div className="mt-3">
-        <label className="block text-left font-medium text-[#00585E] pt-1">Descrição</label>
+        <label className="block text-left font-medium text-[#00585E] pt-1">
+          Descrição
+        </label>
         <input
           type="text"
           value={formData.description}
@@ -16,7 +20,9 @@ const ReminderForm = ({ formData, setFormData, reminderType }) => {
       </div>
 
       <div className="mt-3">
-        <label className="block text-left font-medium text-[#00585E] pt-1">Tipo</label>
+        <label className="block text-left font-medium text-[#00585E] pt-1">
+          Tipo
+        </label>
         <select
           className="border-1 border-gray-300 focus:border-[#00585E] rounded-lg w-full p-2 mt-1 transition-all outline-none"
           value={formData.type}
@@ -32,7 +38,9 @@ const ReminderForm = ({ formData, setFormData, reminderType }) => {
       </div>
 
       <div className="mt-3">
-        <label className="block text-left font-medium text-[#00585E] pt-1">Data</label>
+        <label className="block text-left font-medium text-[#00585E] pt-1">
+          Data
+        </label>
         <input
           type="date"
           value={formData.date}
